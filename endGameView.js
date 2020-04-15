c78b18043074ca055e0aacaeb664d02bbe13eb40
Kamil Grabek <kamil.grabek@allegro.pl>
2020-04-15 11:54:44 +0200

    code refactor - render functions for view parts

--- endGameView.js
+++ endGameView.js
@@ -1,20 +1,43 @@
 function endGameView(state, stateUpdate) {
+  function renderTitle() {
+    const endGameHeader = document.createElement('h1');
+
+    endGameHeader.textContent = 'Game finished!';
+
+    return endGameHeader;
+  }
+
+  function renderScore(state) {
+    const gameScore = document.createElement('h3');
+
+    gameScore.textContent = `You made ${state.mistakes} mistakes`;
+
+    return gameScore;
+  }
+
+  function renderActions(state, stateUpdate) {
+    const container = document.createElement('div');
+    const playAgain = document.createElement('button');
+
+    playAgain.textContent = `Play again`;
+    playAgain.addEventListener('click', () => {
+      stateUpdate({ activeView: 'welcome' });
+    });
+
+    container.append(playAgain);
+
+    return container;
+  }
+
   const viewContent = document.createElement('div');
-  const endGameHeader = document.createElement('h1');
-  endGameHeader.textContent = 'Game finished!';
-  
-  const gameScore = document.createElement('h3');
-  gameScore.textContent = `You made ${state.mistakes} mistakes`;
-  
-  const playAgain = document.createElement('button');
-  playAgain.textContent = `Play again`;
-  playAgain.addEventListener('click', () => {
-    stateUpdate({ activeView: 'welcome' });
-  });
-  
-  viewContent.appendChild(endGameHeader);
-  viewContent.appendChild(gameScore);
-  viewContent.appendChild(playAgain);
+
+  const title = renderTitle();
+  const score = renderScore(state, stateUpdate);
+  const actions = renderActions(state, stateUpdate);
+
+  viewContent.appendChild(title);
+  viewContent.appendChild(score);
+  viewContent.appendChild(actions);
   
   return viewContent;
 }
--- welcomeView.js
+++ welcomeView.js
@@ -1,32 +1,60 @@
 function welcomeView(state, stateUpdate) {
+  function renderTitle() {
+    const viewTitle = document.createElement('h1');
+
+    viewTitle.textContent = `Welcome to Hangman!`;
+
+    return viewTitle
+  }
+
+  function renderNameForm(state, stateUpdate) {
+    const container = document.createElement('div');
+    const nameInputLabel = document.createElement('div');
+    const nameInput = document.createElement('input');
+
+    nameInputLabel.textContent = 'Enter your name';
+    nameInput.addEventListener('input', event => {
+      stateUpdate({ name: event.target.value });
+    });
+    setTimeout(() => {
+      nameInput.value = state.name;
+      nameInput.selectionStart = state.name.length;
+      nameInput.focus();
+    }, 0);
+
+    container.append(nameInputLabel);
+    container.append(nameInput);
+
+    return container;
+  }
+
+  function renderActions(state, stateUpdate) {
+    const container = document.createElement('div');
+    const playButton = document.createElement('button');
+
+    playButton.textContent = 'Play game!';
+    playButton.addEventListener('click', () => {
+      stateUpdate({
+        activeView: 'play',
+        secretPhrase: randomPhrase(),
+        mistakes: 0, selectedLetters: [],
+      });
+    });
+
+    container.append(playButton);
+
+    return container;
+  }
+
   const viewContent = document.createElement('div');
-  const viewTitle = document.createElement('h1');
-  viewTitle.textContent = `Welcome to Hangman!`;
+
+  const title = renderTitle();
+  const nameForm = renderNameForm(state, stateUpdate);
+  const actions = renderActions(state, stateUpdate);
   
-  const nameInputLabel = document.createElement('div');
-  nameInputLabel.textContent = 'Enter your name';
-  
-  
-  const nameInput = document.createElement('input');
-  nameInput.addEventListener('input', event => {
-    stateUpdate({ name: event.target.value });
-  });
-  setTimeout(() => {
-    nameInput.value = state.name;
-    nameInput.selectionStart = state.name.length;
-    nameInput.focus();
-  }, 0);
-  
-  const playButton = document.createElement('button');
-  playButton.textContent = 'Play game!';
-  playButton.addEventListener('click', () => {
-    stateUpdate({ activeView: 'play', secretPhrase: randomPhrase(), mistakes: 0, selectedLetters: [] });
-  });
-  
-  viewContent.appendChild(viewTitle);
-  viewContent.appendChild(nameInputLabel);
-  viewContent.appendChild(nameInput);
-  viewContent.appendChild(playButton);
+  viewContent.appendChild(title);
+  viewContent.appendChild(nameForm);
+  viewContent.appendChild(actions);
   
   return viewContent;
 }
