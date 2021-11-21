0dee7af4a8ca17de24326975549cb84796594adb
Kamil Grabek <kamil.grabek@allegro.pl>
2021-11-21 04:32:00 +0100

    Handling user input

--- game.js
+++ game.js
@@ -8,3 +8,15 @@ const viewTitle = document.createElement('h1');
 viewTitle.textContent = `Welcome to Hangman!`;
 
 gameContent.appendChild(viewTitle);
+
+const nameInputLabel = document.createElement('div');
+nameInputLabel.textContent = 'Enter your name';
+
+const nameInput = document.createElement('input');
+nameInput.addEventListener('blur', event => {
+    console.log(event.target.value);
+});
+
+gameContent.appendChild(viewTitle);
+gameContent.appendChild(nameInputLabel);
+gameContent.appendChild(nameInput);
